794a209ea64eabe5668ecc96bd75b3eb9b2c8518
于战锋 <xinhuo@163.com>
2022-04-13 22:03:28 +0800

    初步打印文件信息,设计参数分析思路,接下来就是递归和字符拆分,分支getFileInfo函数

--- 上机作业/目录遍历/list/main.c
+++ 上机作业/目录遍历/list/main.c
@@ -1,18 +1,40 @@
 #include<stdio.h>
 #include<dirent.h>
 #include<sys/stat.h>
+#include<stdlib.h>
+#include<string.h>
 
 /* i节点属性 */
-struct stat st;
+// struct stat st;
 // ret = stat(path,&st);
-
 /* 判断节点是否为目录 */
 // S_ISDIR(st.st_mode)
 
+
+struct para
+{
+    int _r;
+    int _a;
+    int _l;
+    int _lnum;
+    int _h;
+    int _hnum;
+    int _m;
+    int _mnum;
+};
+
+struct para MODE;
+
+
+//初始化MODE
+void initMode(int argc, char* argv[]);
+
 // 路径 + 打印形式 这里mode应该是结构体
-void manager(char* path, int mode);
-
-
+void manager(char* path, struct para mode);
+// 获取文件详细信息
+int getFileInfo(char* path, struct para mode);
+//详细打印stat结构体
+void showFileInfo(struct stat statbuf);
 
 int main(int argc, char* argv[]) {
     printf("argc:%d\n",argc);
@@ -22,16 +44,27 @@ int main(int argc, char* argv[]) {
     printf("\n");
 
     /* 分析参数 */
-    char* path = "/home/rock/Desktop";
-    int mode = 0;
-
+    char* path = "/bin";
+    struct para mode;
     /* 根据参数结果选择调用的函数 */
     manager(path,mode);
     return 0;
 }
 
 
-void manager(char* path, int mode)
+void initMode(int argc, char* argv[])
+{
+    MODE._r = 0;
+    MODE._a = 0;
+    MODE._h = 0;
+    MODE._hnum = 0;
+    MODE._l = 0;
+    MODE._lnum = 0;
+    MODE._m = 0;
+    MODE._mnum = 0;
+}
+
+void manager(char* path, struct para mode)
 {
     DIR* dirp = NULL;
     dirp = opendir(path);
@@ -43,11 +76,61 @@ void manager(char* path, int mode)
    struct dirent * direntp = NULL;
    direntp = readdir(dirp);
    while(direntp != NULL) {
-       //获取文件信息
-        printf("%s\t",direntp->d_name);
+        // 生成路径信息
+        char dirbuf[512];
+        memset(dirbuf,0,sizeof(dirbuf));
+        strcat(dirbuf,path);
+        strcat(dirbuf,"/");
+        strcat(dirbuf,direntp->d_name);
+       //打印文件信息
+       getFileInfo(dirbuf, mode);
        //更新文件信息
        direntp = readdir(dirp);
    }
    printf("\n");
    closedir(dirp);
+}
+
+int getFileInfo(char* path, struct para mode)
+{
+//    printf("%s\n",path);
+    struct stat statbuf;
+    if(stat(path,&statbuf) == -1) {
+        printf("error\n");
+        return -1;
+    }
+
+    if(S_ISDIR(statbuf.st_mode)) {
+        return -1;
+    }
+
+    if(S_ISREG(statbuf.st_mode)) {
+        printf("%lu\t%s\n", statbuf.st_ino,path);
+    }
+    return 0;
+}
+
+void showFileInfo(struct stat statbuf)
+{
+
+// struct stat {   
+//         mode_t     st_mode;       //文件访问权限   
+//         ino_t      st_ino;       //索引节点号   
+//         dev_t      st_dev;        //文件使用的设备号   
+//         dev_t      st_rdev;       //设备文件的设备号   
+//         nlink_t    st_nlink;      //文件的硬连接数   
+//         uid_t      st_uid;        //所有者用户识别号   
+//         gid_t      st_gid;        //组识别号   
+//         off_t      st_size;       //以字节为单位的文件容量   
+//         time_t     st_atime;      //最后一次访问该文件的时间   
+//         time_t     st_mtime;      //最后一次修改该文件的时间   
+//         time_t     st_ctime;      //最后一次改变该文件状态的时间   
+//         blksize_t st_blksize;    //包含该文件的磁盘块的大小   
+//         blkcnt_t   st_blocks;     //该文件所占的磁盘块   
+// };
+
+    /* 需要的参数 st_ino:unsighed short 以及文件路径*/
+
+    printf("%ld\t",statbuf.st_ino);
+
 }
--- 上机作业/目录遍历/list/readme.md
+++ 上机作业/目录遍历/list/readme.md
@@ -1,5 +1,26 @@
 ### 思路
+先分析参数
+获取参数对应模式,可以使用结构体记录该模式
+然后根据参数调用相应的函数
+现在可以简单进行目录打印
+但是,递归打印的话,还不行
+应该将struct para mode 设置为全局变量,方便递归函数的执行
 
+### 功能
+```sh
+# 基本命令
+ls
+    # 244 myap.c
+ls file
+ls dir
+
+# 自定义选项
+ls -r #这里对应 -R 递归打印 Linux中 -r 是 reverse
+ls -a
+ls -l num -h num
+ls -m
+-- #显式终止命令解析
+```
 ### 参考链接
 - https://blog.csdn.net/zhuyi2654715/article/details/7605051
 
